684cd623a1434ad174b9f94e96f9c723eb3eb416
wmavery <wanda.avery@att.net>
2024-08-05 16:38:53 -0500

    atm dispenser machine

---
+++ Aug5/task2/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Table from './components/table/Table';
+import ATM from './components/atm/ATM';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Table /> */}
+      <ATM />
+    </div>
+  );
+}
+
+export default App;
